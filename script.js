103dec9651f0397f2e2c6fc62cd250462957158b
modernxpunk <modernxpunk@gmail.com>
2022-04-29 13:36:27 +0300

    multiply cubs

--- script.js
+++ script.js
@@ -1,14 +1,47 @@
-const cube = document.querySelector('.cube')
+const container = document.querySelector('.container')
+const settingInputs = document.querySelectorAll('.setting input')
 
-let [rX, rY] = [0, 0]
-let rotateability = false
+let [X, Y] = [0, 0]
+let turn = false
 
-document.addEventListener('mouseup', () => rotateability = false)
-document.addEventListener('mousedown', () => rotateability = true)
+document.addEventListener('mouseup', () => turn = false)
+document.addEventListener('mousedown', () => turn = true)
 document.addEventListener('mousemove', e => {
-	if (rotateability) {
-		rX += e.movementX / 2
-		rY += e.movementY / 2
-		cube.style.transform = `rotateX(${-rY}deg) rotateY(${rX}deg)`
+	if (turn) {
+		X += e.movementX / 2
+		Y += e.movementY / 2
+		container.querySelector('.cube').style.transform = `rotateX(${-Y - 45}deg) rotateY(${X + 35}deg)`
 	}
 })
+
+
+const makeCubeHtml = (html, className="sub cube") => {
+	return `
+		<div class="${className}">
+			<div class="side front"></div>
+			<div class="side back"></div>
+			<div class="side left"></div>
+			<div class="side right"></div>
+			<div class="side bottom"></div>
+			${html}
+		</div>
+	`
+}
+
+const units = {
+	sizeOfCube: "px",
+	offset: "px",
+	perspective: "px",
+	margin: "%"
+}
+
+const makeCubes = count => makeCubeHtml(count <= 0 ? "" : makeCubes(count - 1))
+
+for (let i = 0; i < settingInputs.length; i++) {
+	settingInputs[i].addEventListener('change', e => {
+		if (e.target.name === "count")
+			container.innerHTML = makeCubeHtml(makeCubes(+e.target.value), "cube")
+		else
+			document.documentElement.style.setProperty(`--${e.target.name}`, e.target.value + units[e.target.name])
+	})
+}
